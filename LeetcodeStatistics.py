3b9e052e54789bfb3bbaf0785a237be130da20c9
spriteboysz <sprite_bx@126.com>
2023-09-24 15:17:56 +0800

    2023092415

--- LeetcodeStatistics.py
+++ LeetcodeStatistics.py
@@ -23,15 +23,18 @@ def walk_data():
         for file in filter(lambda f: f.split(".")[-1] in language, files):
             suffix = file.split(".")[-1]
             name = file[:-(len(suffix) + 1)]
+            # 基础算法题
             if file.startswith("P"):
                 name = name.split(".")[0]
                 record1[name][suffix] += 1
+            # 面试题
             elif file.startswith('面'):
                 name = file[:9]
                 record2[name][suffix] += 1
             elif file.startswith('M0'):
                 name = "面试题 " + file[3:5] + "." + file[7:9]
                 record2[name][suffix] += 1
+            # LCP LCR LCS
             elif file.startswith("LC"):
                 if "." in name:
                     name = name.split(".")[0]
@@ -40,27 +43,28 @@ def walk_data():
                 elif file.startswith("LCR"):
                     name = "LCR " + file[4:7]
                 record2[name][suffix] += 1
-            elif file[0] in ['O', '剑']:
-                record2[name][suffix] += 1
+            # 剑指题
+            # elif file[0] in ['O', '剑']:
+            #     record2[name][suffix] += 1
     return record1, record2
 
 
 def to_grid(record):
     rows = sorted(list(record.keys()))
     m, n = len(rows), len(language)
-    grid = [["" for _ in range(n + 2)] for _ in range(m + 1)]
+    grid = [["" for _ in range(n + 2)] for _ in range(m + 2)]
 
     grid[0][0] = "题目列表"
     grid[0][1] = "统计"
     for j, lan in enumerate(language):
         grid[0][j + 2] = lan
     for i, row in enumerate(rows):
-        grid[i + 1][0] = row
-        grid[i + 1][1] = str(len(record[row]))
+        grid[i + 2][0] = row
+        grid[i + 2][1] = str(len(record[row]))
     for i, row in enumerate(rows):
         for j, lan in enumerate(language):
             if record[row][lan] > 0:
-                grid[i + 1][j + 2] = "√"
+                grid[i + 2][j + 2] = "√"
     return grid
 
 
@@ -71,6 +75,9 @@ def to_excel(grids):
             sheet = workbook.sheets.add()
             sheet.range("A1").value = grid
             sheet.autofit()
+            sheet.range("C3").select()
+            workbook.app.api.ActiveWindow.FreezePanes = True
+            # sheet.range("A1").api.AutoFilter(1, "<>", True)
         workbook.save(excel_file)
         workbook.close()
 
